08f8d90f3d3d58d12b7f7752b2dd6f8c52f241b7
Lorenzo Iovino <thisloke@gmail.com>
2023-12-28 11:33:21 +0100

    Added tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MenuComponent} from "./menu/menu.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'iov-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
